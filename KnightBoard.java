79f42bda034795f69283f0027369eab0f1da5d97
cohyunoh <coh00@stuy.edu>
2019-02-13 11:23:32 -0500

    coded the toString method. also added a gitignore

---
+++ KnightBoard.java
@@ -0,0 +1,41 @@
+public class KnightBoard{
+  private int[][] data;
+  public KnightBoard(int l, int w){
+    data = new int[l][w];
+  }
+  public String toString(){
+    String ans = "";
+    if((data.length * data[0].length) % 10 == (data.length * data[0].length)){
+      for(int r = 0; r < data.length; r++){
+        String line = "";
+        for(int c = 0; c < data[r].length; c++){
+          if(data[r][c]==0){
+            line += "_ ";
+          }else{
+            line += data[r][c] + " ";
+          }
+        }
+        line += "\n";
+        ans += line;
+      }
+    }else{
+      for(int r = 0; r < data.length; r++){
+        String line = "";
+        for(int c = 0; c < data[r].length; c++){
+          if(data[r][c]==0){
+            line += "__ ";
+          }else{
+            if(data[r][c] % 10 == data[r][c]){
+              line += "_" + data[r][c] + " ";
+            }else{
+              line += data[r][c] + " ";
+            }
+          }
+        }
+        line += "\n";
+        ans += line;
+      }
+    }
+    return ans;
+  }
+}
