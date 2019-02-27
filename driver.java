e3562e98a18b0ba2769f43dee6df1aa772dc9827
cohyunoh <coh00@stuy.edu>
2019-02-27 00:48:50 -0500

    it works!!

--- driver.java
+++ driver.java
@@ -1,6 +1,6 @@
 public class driver{
   public static void main(String[] args) {
-    KnightBoard board = new KnightBoard(5,4);
+    KnightBoard board = new KnightBoard(7,7);
     System.out.println(board);
     System.out.println(board.countSolutions(0,0));
     System.out.print(board);
